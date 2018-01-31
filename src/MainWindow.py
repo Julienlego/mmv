ed4929e4536707bc03ac5b200539b70f1eac91dd
Julien Gilbert <julienlego@gmail.com>
2018-01-31 12:43:41 -0500

    Separated debug box to its own panel
    Had to copy-paste directly using github in chrome because I didn't merge properly before I pushed my changes.

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -8,8 +8,8 @@ class PygameDisplay(wx.Window):
         wx.Window.__init__(self, parent, ID)
         self.parent = parent
         self.hwnd = self.GetHandle()
-        # if sys.platform == "win32":
-        os.environ['SDL_VIDEODRIVER'] = 'windib'
+        if sys.platform == "win32":
+            os.environ['SDL_VIDEODRIVER'] = 'windib'
         os.environ['SDL_WINDOWID'] = str(self.hwnd)
 
         pygame.display.init()
@@ -20,7 +20,6 @@ class PygameDisplay(wx.Window):
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         self.Bind(wx.EVT_TIMER, self.Update, self.timer)
         self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_IDLE, self.OnIdle)
 
         self.resized = False
 
@@ -42,12 +41,6 @@ class PygameDisplay(wx.Window):
     def OnSize(self, event):
         self.size = self.GetSizeTuple()
 
-    def OnIdle(self, event):
-        if self.resized:
-            print("New size: ", self.GetSize())
-            self.resized = False
-
-
     def Kill(self, event):
         # Make sure Pygame can't be asked to redraw /before/ quitting by unbinding all methods which
         # call the Redraw() method
@@ -55,29 +48,34 @@ class PygameDisplay(wx.Window):
         self.Unbind(event=wx.EVT_PAINT, handler=self.OnPaint)
         self.Unbind(event=wx.EVT_TIMER, handler=self.Update, source=self.timer)
 
+
+class DebugFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, None, title='Debug', size=(300,250))
+        self.parent = parent
+        pan = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        # Create debug text box
+        self.textbox = wx.TextCtrl(pan, -1, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        sizer.Add(self.textbox, 0, flag=wx.EXPAND, border=2)
+        pan.SetSizer(sizer)
+
 class MainFrame(wx.Frame):
     """
     This class is the main window class.
     All of the setup is done in the init function.
     """
 
-    def __init__(self, parent):
-        super(MainFrame, self).__init__(parent, title="Midi Music Visualizer", size=(800, 600))
-        self.panel = wx.Panel(self)
+    def __init__(self, parent, size):
+        super(MainFrame, self).__init__(parent, title="Midi Music Visualizer", size=size)
+
+        # Create the panels
+        topPanel = wx.Panel(self, size=(800, 600))
+        panelPygame = wx.Panel(topPanel, -1, pos=(0,100), size=(250,250))
+        panelDebug = wx.Panel(topPanel, -1, pos=(100,0), size=(250,250))
+
         self.display = PygameDisplay(self, -1)
         self.vizmanager = vm.VizManager(self.display.screen)
-
-        self.SetMinSize((300, 200))  # the frame starts looking weird if it gets too small
-
-        self.InitUI()
-
-        self.Centre()
-        self.Show(True)
-
-    def InitUI(self):
-        """
-        Setup the main frame with all the panels, file menu, and status bar
-        """
 
         # Create menu bar
         menubar = wx.MenuBar()
@@ -93,19 +91,11 @@ class MainFrame(wx.Frame):
         self.statusbar = self.CreateStatusBar()
         self.statusbar.SetFieldsCount(3)
         self.statusbar.SetStatusWidths([-3, -4, -2])
-        self.statusbar.SetStatusText(self.vizmanager.parser.path, 0)
-        self.statusbar.SetStatusText("Look, it's a nifty status bar!!!", 1)
+        self.statusbar.SetStatusText("No file selected", 0)
+        self.statusbar.SetStatusText("No preset selected", 1)
 
         menubar.Append(filemenu, '&File')
         menubar.Append(viewmenu, '&View')
-
-        #qmi = wx.MenuItem(filemenu, wx.ID_EXIT, '&Open\tCtrl+O')
-        #run = wx.MenuItem(filemenu, wx.ID_EXIT, '&Run\tCtrl+R')
-        #filemenu.Append(qmi)
-        #filemenu.Append(run)
-
-        # Create debug text box
-        # self.debugbox = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE|wx.TE_READONLY)
 
         # Bind everything!
         self.Bind(wx.EVT_MENU, self.OnQuit)
@@ -116,12 +106,17 @@ class MainFrame(wx.Frame):
 
         # Add panels to sizer and set to panel
         sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self.display, 0, flag=wx.ALIGN_LEFT|wx.EXPAND|wx.ALL, border=3)
-        # sizer.Add(self.debugbox, 1, flag=wx.ALIGN_RIGHT|wx.EXPAND|wx.ALL, border=3)
+        sizer.Add(panelPygame, 0, flag=wx.EXPAND|wx.ALL, border=10)
+        sizer.Add(panelDebug, 1, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.child = DebugFrame(self)
+        self.child.Show()
 
         self.SetMenuBar(menubar)
         self.SetAutoLayout(True)
-        self.panel.SetSizerAndFit(sizer)
+        topPanel.SetSizerAndFit(sizer)
+        self.Centre()
+        self.Show(True)
 
     def OnQuit(self, event):
         """
@@ -136,8 +131,8 @@ class MainFrame(wx.Frame):
         """
         Called whenever the main frame is re-sized
         """
-        self.display.SetSize(event.GetSize() - (0, 85))  # magic number for now
-        print("Size: ", self.display.size)
+        #self.display.SetSize(event.GetSize())
+        #print("Size: ", self.display.size)
         self.Layout()
 
     def OpenFile(self, event):
@@ -158,9 +153,9 @@ class MainFrame(wx.Frame):
         Shows/Hides the debug textbox
         """
         if self.toggledebug.IsChecked():
-            print("debug show")
+            self.child.Show()
         else:
-            print("debug hide")
+            self.child.Hide()
 
     def PlayVisualization(self, event):
         """
@@ -176,7 +171,7 @@ class MainFrame(wx.Frame):
 
 class App(wx.App):
     def OnInit(self):
-        self.frame = MainFrame(None)
+        self.frame = MainFrame(None, size=(800, 600))
         self.frame.Show()
         self.SetTopWindow(self.frame)
 
